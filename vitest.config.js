823f86990b12652748a3c71f1ba5c817770a0116
ArtlessConstruct <13288347+artlessconstruct@users.noreply.github.com>
2025-05-31 09:49:09 +0100

    Update dev dependencies

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,14 @@ import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
 export default defineWorkersConfig({
     test: {
         include: "./test/*.js",
+        deps: {
+            optimizer: {
+                ssr: {
+                    enabled: true,
+                    include: ["chai"],
+                },
+            },
+        },
         poolOptions: {
             workers: {
                 wrangler: { configPath: "./wrangler.toml" },
